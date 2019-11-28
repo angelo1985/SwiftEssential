6e10236efce95ec6397039dd4ba9300ec8b7f3e0
angelo1985 <56689916+angelo1985@users.noreply.github.com>
2019-11-28 19:35:33 +0100

    Add files via upload

---
+++ SwiftUiESencial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUiESencial
+//
+//  Created by DAM+DM-2019 on 28/11/2019.
+//  Copyright © 2019 DAM+DM-2019. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
