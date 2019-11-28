c81f895b7c38db91971a0134175acab8473b87f5
angelo1985 <56689916+angelo1985@users.noreply.github.com>
2019-11-28 20:57:09 +0100

    imagenCircular

---
+++ SwiftUiESencial/CircleImage.swift
@@ -0,0 +1,28 @@
+//
+//  CircleImage.swift
+//  SwiftUiESencial
+//
+//  Created by DAM+DM-2019 on 28/11/2019.
+//  Copyright © 2019 DAM+DM-2019. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+         Image("turtlerock")
+              .clipShape(Circle())
+            .overlay(
+            Circle().stroke(Color.gray, lineWidth: 4))
+    
+    }
+    
+    
+                  
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
