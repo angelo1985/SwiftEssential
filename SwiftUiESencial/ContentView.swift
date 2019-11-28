211d36708e76715ab88d44df1d383b38cbc8b7b6
angelo1985 <56689916+angelo1985@users.noreply.github.com>
2019-11-28 20:39:16 +0100

    padding

--- SwiftUiESencial/ContentView.swift
+++ SwiftUiESencial/ContentView.swift
@@ -10,12 +10,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack(alignment: .leading) {
+            
+                    Text("Turtle Rock")
+                        .font(.title)
+                        .multilineTextAlignment(.center)
+                        .padding(.leading)
+                    
+                HStack {
+               
+                    Text(/*@START_MENU_TOKEN@*/"Joshua Tree National Park"/*@END_MENU_TOKEN@*/)
+                        .font(.subheadline)
+                        .multilineTextAlignment(.leading)
+                    
+                    Spacer()
+                    
+                    Text("California")
+                        .font(.subheadline)
+                        
+            }
+            .padding()
+            }
+        }
     }
-}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
